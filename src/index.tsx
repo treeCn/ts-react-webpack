85c4eee9357b24adcb6f1f17c35e2855d768633d
kuangshp <kuangshp@126.com>
2019-06-03 10:35:37 +0800

    使用redux本地存储

--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,22 @@ import { Route, Link } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import Counter4 from './components/Counter4';
 import Counter5 from './components/Counter5';
-import store from './store';
+import { store, persistor } from './store';
 import history from './store/history';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <>
-        <Link to="/counter1">counter1</Link> <br />
-        <Link to="/counter2">counter2</Link>
-        <Route path="/counter1" component={Counter4} />
-        <Route path="/counter2" component={Counter5} />
-      </>
-    </ConnectedRouter>
+    <PersistGate persistor={persistor}>
+      <ConnectedRouter history={history}>
+        <>
+          <Link to="/counter1">counter1</Link> <br />
+          <Link to="/counter2">counter2</Link>
+          <Route path="/counter1" component={Counter4} />
+          <Route path="/counter2" component={Counter5} />
+        </>
+      </ConnectedRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -3,8 +3,22 @@ import reducers from './reducers';
 import logger from 'redux-logger';
 import { routerMiddleware } from 'connected-react-router';
 import history from './history';
+// 使用redux本地存储
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // 使用路由中间件
 let router = routerMiddleware(history);
 
-let store = applyMiddleware(logger, router)(createStore)(reducers);
-export default store;
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+let store = applyMiddleware(logger, router)(createStore)(persistedReducer);
+let persistor = persistStore(store)
+export {
+  store,
+  persistor
+};
